bb4c77bb67dd4978d65c1996374f4a09f4f7de5a
Rifqi Rosyidi <rief.rosyidi@gmail.com>
2019-09-20 21:42:26 +0700

    updated some template and change debug variables

--- django_project/settings.py
+++ django_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.getenv("DEBUG_VAL") == 'True')
 
 ALLOWED_HOSTS = ["nostalgic-blog.herokuapp.com"]
 
